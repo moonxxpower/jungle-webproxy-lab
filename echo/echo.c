fdea585f945c26b58e2c8b2147c393cbff0ad6ee
moonxxpower <su551732@gmail.com>
2024-05-07 23:03:44 +0900

    echo.c 주석 수정

--- echo/echo.c
+++ echo/echo.c
@@ -7,7 +7,7 @@ void echo(int connfd) {
 
     Rio_readinitb(&rio, connfd);    // rio 구조체를 초기화하고, connfd에 대한 읽기 작업 수행할 수 있도록 설정 
 
-    // buffer에 저장된 데이터의 크기가 0이 아닐 때까지 반복하여 클라이언트로부터 받은 데이터를 다시 클라이언트로 보내는 역할 수행
+    // buffer에 저장된 데이터의 크기가 0이 아닐 때까지 반복하여 클라이언트로부터 읽은 데이터(Rio_readlineb)를 다시 클라이언트로 전송하는(Rio_writen) 역할을 수행
     while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
         printf("server received %d bytes\n", (int)n);
         Rio_writen(connfd, buf, n);
